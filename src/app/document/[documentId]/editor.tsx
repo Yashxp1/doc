75c64839c25dc50bd6264b0139ebe705e2607057
Yash Raj <kyashraj991@gmail.com>
2025-10-07 21:46:06 +0530

    feat: implement Editor component with Tiptap extensions and custom styles

---
+++ src/app/document/[documentId]/editor.tsx
@@ -0,0 +1,89 @@
+'use client';
+
+import { TaskItem, TaskList } from '@tiptap/extension-list';
+import { useEditor, EditorContent } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
+import {
+  Table,
+  TableCell,
+  TableHeader,
+  TableRow,
+} from '@tiptap/extension-table';
+import { useEditorStore } from '@/store/user-editor-store';
+import { TextStyle, FontFamily } from '@tiptap/extension-text-style';
+
+const Editor = () => {
+  const { setEditor } = useEditorStore();
+
+  const editor = useEditor({
+    onCreate({ editor }) {
+      setEditor(editor);
+    },
+    onDestroy() {
+      setEditor(null);
+    },
+    onUpdate({ editor }) {
+      setEditor(editor);
+    },
+    onSelectionUpdate({ editor }) {
+      setEditor(editor);
+    },
+    onTransaction({ editor }) {
+      setEditor(editor);
+    },
+    onFocus({ editor }) {
+      setEditor(editor);
+    },
+    onBlur({ editor }) {
+      setEditor(editor);
+    },
+    onContentError({ editor }) {
+      setEditor(editor);
+    },
+    editorProps: {
+      attributes: {
+        style: 'padding-left: 56px; padding-right: 56px',
+        class:
+          'focus:outline-none print-border-0 bg-white border border-[#c7c7c7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text',
+      },
+    },
+    extensions: [
+      StarterKit,
+      FontFamily,
+      TextStyle,
+      Table,
+      TableCell,
+      TableRow,
+      TableHeader,
+      TaskItem.configure({ nested: true }),
+      TaskList,
+    ],
+    content: `
+      <table>
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th colspan="3">Description</th>
+          </tr>
+          <tr>
+            <td>Cyndi Lauper</td>
+            <td>Singer</td>
+            <td>Songwriter</td>
+            <td>Actress</td>
+          </tr>
+        </tbody>
+      </table>
+    `,
+    immediatelyRender: false,
+  });
+
+  return (
+    <div className="size-full overflow-x-auto bg-[#f9fbfd] px-4 print:p-0 print:bg-white print:overflow-visible">
+      <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
+        <EditorContent editor={editor} />
+      </div>
+    </div>
+  );
+};
+
+export default Editor;
