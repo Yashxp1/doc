a961efb8fcc24c281cac1912138cb986bc496bca
Yash Raj <kyashraj991@gmail.com>
2025-10-08 21:13:43 +0530

    feat: restructure components

--- src/app/document/[documentId]/page.tsx
+++ src/app/document/[documentId]/page.tsx
@@ -2,13 +2,19 @@
 
 import React from 'react';
 import Editor from './editor';
-import {Toolbar} from './Toolbar';
+import { Toolbar } from './Toolbar';
+import { Navbar } from './Navbar';
 
 const Page = () => {
   return (
     <div className="min-h-screen bg-[#fafbfd]">
-      <Toolbar/>
-      <Editor />
+      <div className="flex flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 z-10 right-0 bg-[#FAFBFD] print:hidden">
+        <Navbar />
+        <Toolbar />
+      </div>
+      <div className="pt-[114px] print:pt-0">
+        <Editor />
+      </div>
     </div>
   );
 };
