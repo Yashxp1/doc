b6f21ee825e8ed20849579ed49f414d7c9c18dcd
Yash Raj <kyashraj991@gmail.com>
2025-10-10 02:22:47 +0530

    feat: add templates for document generation

---
+++ src/constants/templates.ts
@@ -0,0 +1,29 @@
+export const templates = [
+  { id: 'blank', label: 'Bank Document', imageUrl: '/logo.svg' },
+  {
+    id: 'Software-proposal',
+    label: 'Software development',
+    imageUrl: '/logo.svg',
+  },
+  {
+    id: 'project-proposal',
+    label: 'Project development',
+    imageUrl: '/logo.svg',
+  },
+  {
+    id: 'Business-letter',
+    label: 'Business-letter',
+    imageUrl: '/logo.svg',
+  },
+  { id: 'Resume', label: 'Resume', imageUrl: '/logo.svg' },
+  {
+    id: 'cover-letter',
+    label: 'Cover letter',
+    imageUrl: '/logo.svg',
+  },
+  {
+    id: 'letter',
+    label: 'Letter',
+    imageUrl: '/logo.svg',
+  },
+];
